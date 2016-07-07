1b35aa0d1b07e139adbe07220e24a00d10e7abfe
SandBoxDeveloper <madetobethebest@hotmail.co.uk>
2016-07-07 20:58:13 +0100

    Added Unit Test Suite.

---
+++ app/src/test/java/com/hulldiscover/zeus/marvelcomics/UnitTestSuite.java
@@ -0,0 +1,17 @@
+package com.hulldiscover.zeus.marvelcomics;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Test Suite to run all test.
+ */
+
+// Runs all unit tests.
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ExampleUnitTest.class})
+
+public class UnitTestSuite {}
+
+
