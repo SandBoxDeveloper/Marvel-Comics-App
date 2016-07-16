f9e11a3a8f6095d1c6a46afcac1a4c6d1c7c9acd
SandBoxDeveloper <madetobethebest@hotmail.co.uk>
2016-07-16 14:47:55 +0100

    Update project files with: - Content Provider - Contract - SQLite Database

---
+++ app/src/androidTest/java/com/hulldiscover/zeus/marvelcomics/data/FullTestSuite.java
@@ -0,0 +1,16 @@
+package com.hulldiscover.zeus.marvelcomics.data;
+
+import junit.framework.TestSuite;
+
+
+/**
+ * Created by Zeus on 16/07/16.
+ */
+public class FullTestSuite extends TestSuite {
+    /*public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }*/
+
+    public FullTestSuite() { super(); }
+}
